8fc5add82ed31636f583df5ae4954e5f188258d6
BountyHunter <stormcrafterbg@gmail.com>
2021-11-14 20:53:19 +0200

    An attempt was made

---
+++ Semester 1/Week6/GradedHomework2/Task3/Task3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int minStepsToAim = INT_MAX;
+
+void numberOfStepsToAim(const unsigned n, const unsigned aim) 
+{
+    
+}
+
+int main()
+{
+    unsigned n;
+    unsigned aim;
+
+    cin >> n;
+    cin >> aim;
+
+    numberOfStepsToAim(n,aim);
+
+    //cout << minStepsToAim;
+}
