357282f5bdf1ec151c442dfad398ce8393ba8395
Svetlozar S <stormcrafterbg@gmail.com>
2022-10-17 13:58:02 +0300

    Implemented binary, ternary and jump search algorithms

--- Semester3-SDA/Algorithms/TestField/TestField.cpp
+++ Semester3-SDA/Algorithms/TestField/TestField.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include "Algorithms/SortingAlgorithms.h"
+#include "Algorithms/SearchingAlgorithms.h"
 
-void print(const vector<int> &arr) {
-	for (int i = 0; i < arr.size(); i++)
+void print(vector<int> &arr) {
+	for (uint64_t i = 0; i < arr.size(); i++)
 	{
 		cout << arr[i] << " ";
 	}
@@ -11,11 +12,10 @@ void print(const vector<int> &arr) {
 
 int main()
 {
-    //vector<int> arr = { 5, -3, 4, -5, 2, 1, -4, 0, 3, -2, -1 };
-	vector<int> arr = { 132, 312, 52342, 5436, 3543, 3234, 6234, 745, 3123 };
+    vector<int> arr = { 5, -3, 4, -5, 2, 1, -4, 0, 3, -2, -1 };
+	//vector<int> arr = { 132, 312, 52342, 5436, 3543, 3234, 6234, 745, 3123 };
 
-    
-	radixSort(arr);
+	quicksort(arr);
 
-	print(arr);
+	cout << quickselectNthNum(arr, 11);
 }
